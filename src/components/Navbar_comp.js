b4d358bfcce85662046e739377d16559a2d23068
fatemehchahardoli <chahardolifatemehb@gmail.com>
2023-06-14 02:31:10 +0330

    start project

---
+++ src/components/Navbar_comp.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Navbar_comp extends Component {
+   state = {};
+   render() {
+      return (
+         <>
+            <h3>hhhh</h3>
+         </>
+      );
+   }
+}
+
+export default Navbar_comp;
