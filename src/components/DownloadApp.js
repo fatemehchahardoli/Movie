dafdf25f13fb845fa4a818f99e7517d5d6398367
fatemehchahardoli <chahardolifatemehb@gmail.com>
2023-06-18 15:17:08 +0330

    finish dowload app

--- src/components/DownloadApp.js
+++ src/components/DownloadApp.js
@@ -1,9 +1,34 @@
 import React, { Component } from "react";
 import "../styles/downloadApp.css";
+import img1 from "../images/laptop.png";
 class DownloadApp extends Component {
    state = {};
    render() {
-      return <></>;
+      return (
+         <>
+            <div className="display-grid">
+               <div className="align-self-n">
+                  <p>Download Our App</p>
+                  <h2>Watch Any Where</h2>
+                  <p>
+                     Install Cinet app for your device and enjoy high-quality
+                     movies and tv shows anywhere.
+                  </p>
+                  <button>
+                     <span>Android app on</span>
+                     <i className="fa fa-google"></i>Google
+                  </button>
+                  <button>
+                     <span>Available on the</span>
+                     <i className="fa fa-apple"></i>App Store
+                  </button>
+               </div>
+               <div>
+                  <img src={img1} alt="laptop"></img>
+               </div>
+            </div>
+         </>
+      );
    }
 }
 
