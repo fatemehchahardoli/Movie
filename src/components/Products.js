8f9a208a165204a29616861ae5237d538a78c944
fatemehchahardoli <chahardolifatemehb@gmail.com>
2023-06-25 23:42:42 +0330

    search box

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,30 +1,57 @@
 import React, { Component } from "react";
 import "../styles/products.css";
 import Product from "./Product";
-import img1 from "../images/product/product-14.jpg";
-import img2 from "../images/product/product-18.jpg";
-import img3 from "../images/product/product-24.jpg";
-import img4 from "../images/product/product-22.jpg";
 import axios from "axios";
 class products extends Component {
    constructor(props) {
       super(props);
       this.state = {
          movies: [],
+         filterMovies: [],
+         genre: [],
       };
    }
 
    componentDidMount() {
       this.gatData();
-      console.log(this.state);
    }
    gatData = () => {
-      axios
-         .get("/movies.json")
-         .then((Response) => this.setState({ movies: Response.data }));
+      axios.get("/movies.json").then((Response) =>
+         this.setState({
+            movies: Response.data,
+            filterMovies: Response.data,
+         })
+      );
    };
+   showSearchBox = () => {
+      let search = document.querySelectorAll(".search-box");
+      search.forEach((element) => {
+         element.classList.toggle("d-block");
+      });
+   };
+   searchValue = (event) => {
+      if (event.target.value !== "") {
+         let filterResult = this.state.movies.filter((item) => {
+            return item.Title.toLowerCase().includes(event.target.value);
+         });
+         console.log(filterResult);
+         this.setState({ filterMovies: filterResult });
+      } else {
+         this.setState({ filterMovies: this.state.movies });
+      }
+   };
+   listGenre = () => {
+      const genreSet = new Set();
+      this.state.movies.forEach((item) => {
+         const genreList = item.Genre.split(", ");
+         genreList.forEach((genre) => genreSet.add(genre));
+      });
+      this.setState({ genre: [...genreSet] });
+   };
+
    render() {
-      const { movies } = this.state;
+      const { movies, filterMovies, genre } = this.state;
+
       return (
          <>
             <main>
@@ -35,6 +62,9 @@ class products extends Component {
                <section className="filter-product ">
                   <div className="container flex">
                      <div className="filter-left flex ">
+                        {/* {genre.forEach((g) => {
+                           console.log("gg");
+                        })} */}
                         <button>Recommended</button>
                         <button>Top Rated</button>
                         <button>New Arrival</button>
@@ -45,28 +75,37 @@ class products extends Component {
                            <i className="fa fa-filter"> </i>
                            <span className="lable">Filter</span>
                         </div>
-                        <div className="search flex">
+
+                        <div
+                           className="search flex"
+                           onClick={this.showSearchBox}
+                        >
                            <i className="fa fa-search"></i>
 
                            <span className="lable">Search</span>
+                        </div>
+
+                        <div className="search-box">
+                           <input
+                              type="search"
+                              id="search"
+                              name="search"
+                              placeholder="Enter your Movies Name"
+                              className="search-box"
+                              onChange={this.searchValue}
+                           ></input>
                         </div>
                      </div>
                   </div>
                </section>
                <section className="grid">
-                  {movies.map((m) => {
-                     console.log(m.Title);
+                  {filterMovies.map((m) => {
                      return <Product image={m.Poster} Title={m.Title} />;
                   })}
-
-                  {/* <Product image={img2}></Product>
-                  <Product image={img3}></Product>
-                  <Product image={img4}></Product>
-                  <Product image={img1}></Product>
-                  <Product image={img2}></Product> */}
                </section>
                <button className="more">load more</button>
             </main>
+            {this.listGenre()}
          </>
       );
    }
